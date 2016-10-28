3c818be03c636aba541e9c4c80c30d132139994f
Mike Erickson <merickson@cirruspath.com>
2016-10-28 14:16:32 -0700

    add linting

--- test/hello-world.spec.js
+++ test/hello-world.spec.js
@@ -1,11 +1,14 @@
+/*global require, describe, it*/
+
 var assert = require('assert');
 
-describe('sample test', () => {
-  it('should pass', () => {
+describe('sample test', function () {
+  it('should pass', function () {
     assert(true === true);
   });
 
-  it('should fail', () => {
-    assert(true === false);
+  it('should fail (this is done on purpose to assure we handle fail)', function () {
+    assert(true);
   });
+
 });
